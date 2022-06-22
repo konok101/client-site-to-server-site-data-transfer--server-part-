3dc27a9a314b53a06c28552d112e23198c3fc24e
konok101 <rockykonok@gmail.com>
2022-06-23 05:18:10 +0600

    server site delete part done

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 const app = express();
+const ObjectId= require('mongodb').ObjectId;
 const port = process.env.PORT || 5000;
 
 //for acces data
@@ -39,6 +40,15 @@ async function run() {
             console.log(newUser);
             const result = await userCollection.insertOne(newUser);
             res.send(result);
+        });
+
+        //Delete user
+        app.delete('/user/:id', async(req, res)=>{
+            const id= req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
+
         })
     }
     finally {
